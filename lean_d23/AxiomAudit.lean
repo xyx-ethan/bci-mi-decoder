caf0fb8cd2f291cc808c186684905d84c93799d5
xyx-ethan <xyx.ethan@gmail.com>
2026-09-05 19:31:43 -0400

    D23 round21: add axiom audit

---
+++ lean_d23/AxiomAudit.lean
@@ -0,0 +1,6 @@
+import D23Round21
+
+#print axioms D23Round21.normSq_eq_zero_iff
+#print axioms D23Round21.fullRank_scalar_norm_lt_sixteen
+#print axioms D23Round21.smallGaussian_norm_cases
+#print axioms D23Round21.fullRank_scalar_norm_cases
