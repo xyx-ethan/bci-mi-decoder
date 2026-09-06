411ba1367237dbb3668e348cc8e3d1635fadb80c
xyx-ethan <xyx.ethan@gmail.com>
2026-09-05 20:59:06 -0400

    D17 round23: audit corrected and inherited order theorems

--- lean_d17/AxiomAudit.lean
+++ lean_d17/AxiomAudit.lean
@@ -1,7 +1,7 @@
 import D17FiniteY
 
 /-!
-# D17 Round 22 dependency audit
+# D17 Round 23 dependency audit
 -/
 
 #print axioms D17Round22.phi6_pos
@@ -10,6 +10,9 @@ import D17FiniteY
 #print axioms D17Round22.R3_lt_U
 #print axioms D17Round22.R4_lt_U
 #print axioms D17Round22.U_antitone
+#print axioms D17Round22.ratio_product_lt_cube_of_min
 #print axioms D17Round22.ratio_product_lt_cube
 #print axioms D17Round22.boundary_cube
+#print axioms D17Round22.finite_y_bound_of_min
 #print axioms D17Round22.finite_y_bound
+#print axioms D17Round22.finite_y_bound_remaining_order
