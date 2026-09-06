ba183323ad23fe128e9dfb94c680194e41f412ae
xyx-ethan <xyx.ethan@gmail.com>
2026-09-05 20:08:04 -0400

    D14 audit: add axiom audit

---
+++ lean_d14/AxiomAudit.lean
@@ -0,0 +1,8 @@
+import D14Witness
+
+#print axioms D14Round11.factorList_N
+#print axioms D14Round11.a_N
+#print axioms D14Round11.period_remainder
+#print axioms D14Round11.powerN_congruence
+#print axioms D14Round11.exists_odd_witness
+#print axioms D14Round11.conjecture3_true_iff
